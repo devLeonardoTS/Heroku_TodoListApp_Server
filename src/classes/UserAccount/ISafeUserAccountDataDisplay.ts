b76a9b10751f06f05c8feeb2b2d8bb31d44bd57d
Leonardo T. S <66446331+devLeonardoTS@users.noreply.github.com>
2021-12-06 16:01:26 -0300

    Update schema.prisma and user account creation response.

--- src/classes/UserAccount/ISafeUserAccountDataDisplay.ts
+++ src/classes/UserAccount/ISafeUserAccountDataDisplay.ts
@@ -1,4 +1,5 @@
 export interface ISafeUserAccountDataDisplay {
+    id: string | null;
     username: string | null;
     role: string | null;
     createdAt: string | null;
--- src/classes/UserAccount/SafeUserAccountDataDisplay.ts
+++ src/classes/UserAccount/SafeUserAccountDataDisplay.ts
@@ -2,6 +2,7 @@ import { UserAccount } from ".prisma/client";
 import { ISafeUserAccountDataDisplay } from "./ISafeUserAccountDataDisplay";
 
 export class SafeUserAccountDataDisplay implements ISafeUserAccountDataDisplay {
+    id: string | null;
     username: string | null;
     role: string | null;
     createdAt: string | null;
@@ -9,8 +10,9 @@ export class SafeUserAccountDataDisplay implements ISafeUserAccountDataDisplay {
     lastLogin: string | null;
 
     constructor(data: UserAccount){
-        const {username, role, createdAt, updatedAt, lastLogin} = data;
+        const {id, username, role, createdAt, updatedAt, lastLogin} = data;
 
+        this.id = id || null;
         this.username = username || null;
         this.role = role || null;
         this.createdAt = createdAt.toString() || null;
